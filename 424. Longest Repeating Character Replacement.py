b95625eb3de6f4c68912766e2ddd55ff4fe69649
Harshit Patel <harshit.p.patel96@gmail.com>
2021-01-27 16:50:56 -0500

    Update 424. Longest Repeating Character Replacement.py

--- 424. Longest Repeating Character Replacement.py
+++ 424. Longest Repeating Character Replacement.py
@@ -12,3 +12,21 @@ class Solution:
                     counts[s[i-maxFrequency-k]] -= 1 # we have to decrement the frequency because if we do not decrement the frequency and the same char appears in future then it would altercate maxFreq. if there are more than k numbers between the start char and that later appearing char then it is not possible to have that long string
                     
         return min(len(s), maxFrequency + k)
+
+# better variable names
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+        counts = collections.defaultdict(int)
+        maxFrequency = 0
+        for i, char in enumerate(s):
+            counts[char] += 1
+            if counts[char] > maxFrequency:
+                maxFrequency = counts[char]
+            else:
+                start = s[i - maxFrequency - k]
+                currLen = maxFrequency + k
+                if i >= currLen:
+                    counts[start] -= 1
+                    
+        return min(len(s), maxFrequency+k)
