9221b64e32f6ff35f29adf12624366f8137f1293
Harshit Patel <harshit.p.patel96@gmail.com>
2021-01-28 23:16:16 -0500

    Update 236. Lowest Common Ancestor of a Binary Tree.py

--- 236. Lowest Common Ancestor of a Binary Tree.py
+++ 236. Lowest Common Ancestor of a Binary Tree.py
@@ -8,21 +8,18 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
 
-        pathtop =self.getPath(root, p)
+        pathtop = self.getPath(root, p)
         pathtoq = self.getPath(root, q)
-
-        i, j = 0, 0
+        if len(pathtop) > len(pathtoq): i, j = len(pathtop) - len(pathtoq), 0
+        else:  i, j = 0, len(pathtoq) - len(pathtop)
         while i < len(pathtop) and j < len(pathtoq):
-            if pathtop[i] != pathtoq[j]:
-                return pathtop[i-1]
+            if pathtop[i] == pathtoq[j]:
+                return pathtop[i]
             i += 1
             j += 1
-
-        if i == len(pathtop):
-            return pathtop[i-1]
-        return pathtoq[j-1]
-
-            
+        
+        return pathtop[i-1]
+    
     def getPath(self, root, node):
         if not root:
             return
@@ -30,11 +27,14 @@ class Solution:
             return [root]
         
         left = self.getPath(root.left, node)
+        
         if left:
-            return [root] + left
+            left.append(root)
+            return left
         right = self.getPath(root.right, node)
         if right:
-            return [root] + right
+            right.append(root)
+            return right
         
         return
         
