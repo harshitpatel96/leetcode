fab18751d90f079c395e8663904e4adadd7728b1
Harshit Patel <harshit.p.patel96@gmail.com>
2021-02-19 13:42:04 -0500

    Update 169. Majority Element.js

--- 169. Majority Element.js
+++ 169. Majority Element.js
@@ -23,3 +23,32 @@ var majorityElement = function(nums) {
     
     return mod;
 };
+
+/*
+Python
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        """
+        2 3 3
+          i
+        maxCount = 1
+        mod = 2
+        
+        """
+        nums.sort()
+        n = len(nums)
+        mod = nums[0]
+        maxCount = 1
+        currCount = 1
+        for i in range(1, n+1):
+            if i < n and nums[i] == nums[i-1]:
+                currCount += 1
+            else:
+                if currCount > maxCount:
+                    maxCount = currCount
+                    mod = nums[i-1]
+                currCount = 1
+        
+        return mod
+                    
+*/
