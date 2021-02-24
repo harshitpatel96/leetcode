e45a43473dc9a347006057f95590fdf3de2f7f47
Harshit Patel <harshit.p.patel96@gmail.com>
2021-02-24 01:55:54 -0500

    Create 359. Logger Rate Limiter.py

---
+++ 359. Logger Rate Limiter.py
@@ -0,0 +1,29 @@
+class Logger:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.prevMessages = {}
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        """
+        Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity.
+        """
+        if message not in self.prevMessages:
+            self.prevMessages[message] = timestamp+10
+            return True
+        
+        if self.prevMessages[message] <= timestamp:
+            self.prevMessages[message] = timestamp+10
+            return True
+        
+        return False
+        
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
